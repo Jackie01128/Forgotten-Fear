66b31cc1c448ce5f4b9c3c18740c1f904fe49f68
jasonng1 <kar_ng2000@knights.ucf.edu>
2023-10-16 04:24:53 -0400

    Added Stamina bar, Need to tweak speed
    Need to make the enemy move around wall, currently stuck behind wall.

--- Assets/Scripts/SC_FPSController.cs
+++ Assets/Scripts/SC_FPSController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(CharacterController))]
 
@@ -10,13 +11,26 @@ public class SC_FPSController : MonoBehaviour
     public float runningSpeed = 11.5f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
+    public float crouchSpeed = 4.0f;
+    public float crouchHeight = 1.0f;
     public Camera playerCamera;
     public float lookSpeed = 2.0f;
     public float lookXLimit = 45.0f;
+    public float maxStamina = 10.0f;
+    public float staminaRegenerationRate = 1.0f;
+    public Slider staminaSlider;
 
     CharacterController characterController;
     Vector3 moveDirection = Vector3.zero;
     float rotationX = 0;
+    float standingHeight;
+    bool isCrouching = false;
+    float cameraHeightOffset = 0.0f;
+    float currentStamina;
+    private bool canSprint = true;
+    private bool isSprinting = false;
+    private float targetSpeed;
+    private float minSpeed = 4.0f; // Set a minimum speed (e.g., walking speed)
 
     [HideInInspector]
     public bool canMove = true;
@@ -24,21 +38,61 @@ public class SC_FPSController : MonoBehaviour
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        standingHeight = characterController.height;
+        cameraHeightOffset = playerCamera.transform.localPosition.y;
+        currentStamina = maxStamina;
+        targetSpeed = walkingSpeed;
 
-        // Lock cursor
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     void Update()
     {
-        // We are grounded, so recalculate move direction based on axes
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
-        // Press Left Shift to run
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+
+        // Detect crouch input
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isCrouching = !isCrouching;
+
+            // Update camera height and character controller height simultaneously
+            float targetHeight = isCrouching ? crouchHeight : standingHeight;
+            float targetCameraHeight = isCrouching ? cameraHeightOffset - 0.5f : cameraHeightOffset;
+
+            characterController.height = targetHeight;
+            Vector3 cameraPosition = playerCamera.transform.localPosition;
+            cameraPosition.y = targetCameraHeight;
+            playerCamera.transform.localPosition = cameraPosition;
+        }
+
+        // Calculate the player's speed based on current stamina
+        float speedMultiplier = currentStamina / maxStamina;
+
+        // Update the target speed based on sprinting and crouching status
+        if (isSprinting)
+        {
+            targetSpeed = runningSpeed;
+        }
+        else if (isCrouching)
+        {
+            targetSpeed = crouchSpeed;
+        }
+        else
+        {
+            targetSpeed = walkingSpeed;
+        }
+
+        // Ensure that the speed never drops below the minimum speed
+        if (speedMultiplier * targetSpeed < minSpeed)
+        {
+            targetSpeed = minSpeed / speedMultiplier;
+        }
+
+        float curSpeedX = canMove ? targetSpeed * speedMultiplier * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? targetSpeed * speedMultiplier * Input.GetAxis("Horizontal") : 0;
+
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
@@ -51,16 +105,47 @@ public class SC_FPSController : MonoBehaviour
             moveDirection.y = movementDirectionY;
         }
 
-        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-        // as an acceleration (ms^-2)
         if (!characterController.isGrounded)
         {
             moveDirection.y -= gravity * Time.deltaTime;
         }
 
-        // Move the controller
-        characterController.Move(moveDirection * Time.deltaTime);
+        // Detect running input
+        isSprinting = Input.GetKey(KeyCode.LeftShift) && !isCrouching && currentStamina > 0 && canSprint;
+
+        // Enable or disable the Stamina Slider based on sprinting status
+        if (staminaSlider != null)
+        {
+            if (isSprinting)
+            {
+                // Deduct stamina when running
+                currentStamina -= 2.0f * Time.deltaTime;
+
+                // Ensure currentStamina does not go below 0
+                currentStamina = Mathf.Max(0.0f, currentStamina);
+
+                // Update the Stamina Slider value
+                staminaSlider.value = currentStamina / maxStamina;
+                staminaSlider.gameObject.SetActive(true);
+
+                // Transition from sprint to walking when stamina is low
+                if (currentStamina < 0.1f)
+                {
+                    isSprinting = false;
+                }
+            }
+            else
+            {
+                // Regenerate stamina when not running
+                currentStamina += staminaRegenerationRate * Time.deltaTime;
+
+                // Ensure currentStamina does not exceed maxStamina
+                currentStamina = Mathf.Clamp(currentStamina, 0.0f, maxStamina);
+
+                // Hide the Stamina Slider when not running
+                staminaSlider.gameObject.SetActive(false);
+            }
+        }
 
         // Player and Camera rotation
         if (canMove)
@@ -70,5 +155,11 @@ public class SC_FPSController : MonoBehaviour
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
+
+        // Debug logs
+        Debug.Log("Current Speed: " + curSpeedX);
+
+        // Apply character movement
+        characterController.Move(moveDirection * Time.deltaTime);
     }
 }
