4e822210272e69119d6349e10b4bb3893a126174
FabioKok <fabiokch1@outlook.com>
2023-11-28 20:36:47 -0500

    added code state machine

---
+++ Assets/AiAgent.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiAgent : MonoBehaviour
+{
+    public AiStateMachine stateMachine;
+    public AiStateId initalState;
+    public NavMeshAgent navMeshAgent;
+    public AiAgentConfig config;
+    public Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        stateMachine = new AiStateMachine(this);
+        stateMachine.RegisterState(new AiChasePlayerState());
+        stateMachine.RegisterState(new AiIdleState());
+        stateMachine.ChangeState(initalState);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stateMachine.Update();
+    }
+}
--- Assets/AiLocomotion.cs
+++ Assets/AiLocomotion.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AiLocomotion : MonoBehaviour
 {
-    public Transform playerTransform;
-    UnityEngine.AI.NavMeshAgent agent;
+    
+
+    NavMeshAgent agent;
+    Animator animator;
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
-        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = playerTransform.position;
+         
+        animator.SetFloat("Speed", agent.velocity.magnitude);
     }
 }
